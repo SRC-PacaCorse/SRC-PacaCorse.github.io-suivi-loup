e9ef08522bba472dcc98cd0fcc28b82ac1d5fc61
SRC-PacaCorse <135326821+SRC-PacaCorse@users.noreply.github.com>
2024-01-17 18:49:28 +0100

    Add files via upload

---
+++ layers/layers.js
@@ -0,0 +1,101 @@
+ol.proj.proj4.register(proj4);
+ol.proj.get("EPSG:2154").setExtent([784957.768187, 6199925.092478, 1338467.016030, 6535925.092478]);
+var wms_layers = [];
+
+var lyr_ScanRgionalFXXWM082021_0 = new ol.layer.Tile({
+                            source: new ol.source.TileWMS(({
+                              url: "https://wxs.ign.fr/cartes/geoportail/r/wms?VERSION%3D1.3.0",
+    attributions: ' ',
+                              params: {
+                                "LAYERS": "SCANREG_PYR-JPEG_WLD_WM",
+                                "TILED": "true",
+                                "VERSION": "1.3.0"},
+                            })),
+                            title: "Scan Régional FXX WM 08-2021",
+                            opacity: 1.000000,
+                            
+                            
+                          });
+              wms_layers.push([lyr_ScanRgionalFXXWM082021_0, 0]);
+var format_DPT_AURA_PACA_1 = new ol.format.GeoJSON();
+var features_DPT_AURA_PACA_1 = format_DPT_AURA_PACA_1.readFeatures(json_DPT_AURA_PACA_1, 
+            {dataProjection: 'EPSG:4326', featureProjection: 'EPSG:2154'});
+var jsonSource_DPT_AURA_PACA_1 = new ol.source.Vector({
+    attributions: ' ',
+});
+jsonSource_DPT_AURA_PACA_1.addFeatures(features_DPT_AURA_PACA_1);
+var lyr_DPT_AURA_PACA_1 = new ol.layer.Vector({
+                declutter: true,
+                source:jsonSource_DPT_AURA_PACA_1, 
+                style: style_DPT_AURA_PACA_1,
+                interactive: true,
+                title: '<img src="styles/legend/DPT_AURA_PACA_1.png" /> DPT_AURA_PACA'
+            });
+var format_ZPP_ETE_2023_2 = new ol.format.GeoJSON();
+var features_ZPP_ETE_2023_2 = format_ZPP_ETE_2023_2.readFeatures(json_ZPP_ETE_2023_2, 
+            {dataProjection: 'EPSG:4326', featureProjection: 'EPSG:2154'});
+var jsonSource_ZPP_ETE_2023_2 = new ol.source.Vector({
+    attributions: ' ',
+});
+jsonSource_ZPP_ETE_2023_2.addFeatures(features_ZPP_ETE_2023_2);
+var lyr_ZPP_ETE_2023_2 = new ol.layer.Vector({
+                declutter: true,
+                source:jsonSource_ZPP_ETE_2023_2, 
+                style: style_ZPP_ETE_2023_2,
+                interactive: true,
+    title: 'ZPP_ETE_2023<br />\
+    <img src="styles/legend/ZPP_ETE_2023_2_0.png" /> ZPP<br />\
+    <img src="styles/legend/ZPP_ETE_2023_2_1.png" /> Secteur à surveiller<br />'
+        });
+var format_Maillage_Effort_mis_a_jour_le_17_janvier_2024_3 = new ol.format.GeoJSON();
+var features_Maillage_Effort_mis_a_jour_le_17_janvier_2024_3 = format_Maillage_Effort_mis_a_jour_le_17_janvier_2024_3.readFeatures(json_Maillage_Effort_mis_a_jour_le_17_janvier_2024_3, 
+            {dataProjection: 'EPSG:4326', featureProjection: 'EPSG:2154'});
+var jsonSource_Maillage_Effort_mis_a_jour_le_17_janvier_2024_3 = new ol.source.Vector({
+    attributions: ' ',
+});
+jsonSource_Maillage_Effort_mis_a_jour_le_17_janvier_2024_3.addFeatures(features_Maillage_Effort_mis_a_jour_le_17_janvier_2024_3);
+var lyr_Maillage_Effort_mis_a_jour_le_17_janvier_2024_3 = new ol.layer.Vector({
+                declutter: true,
+                source:jsonSource_Maillage_Effort_mis_a_jour_le_17_janvier_2024_3, 
+                style: style_Maillage_Effort_mis_a_jour_le_17_janvier_2024_3,
+                interactive: true,
+    title: 'Maillage_Effort +_mis_a_jour_le_17_janvier_2024<br />\
+    <img src="styles/legend/Maillage_Effort_mis_a_jour_le_17_janvier_2024_3_0.png" /> 0<br />\
+    <img src="styles/legend/Maillage_Effort_mis_a_jour_le_17_janvier_2024_3_1.png" /> 1 - 5<br />\
+    <img src="styles/legend/Maillage_Effort_mis_a_jour_le_17_janvier_2024_3_2.png" /> 6 - 8<br />\
+    <img src="styles/legend/Maillage_Effort_mis_a_jour_le_17_janvier_2024_3_3.png" /> >8<br />'
+        });
+var format_Maillage_Effort_mis_a_jour_le_17_janvier_2024_4 = new ol.format.GeoJSON();
+var features_Maillage_Effort_mis_a_jour_le_17_janvier_2024_4 = format_Maillage_Effort_mis_a_jour_le_17_janvier_2024_4.readFeatures(json_Maillage_Effort_mis_a_jour_le_17_janvier_2024_4, 
+            {dataProjection: 'EPSG:4326', featureProjection: 'EPSG:2154'});
+var jsonSource_Maillage_Effort_mis_a_jour_le_17_janvier_2024_4 = new ol.source.Vector({
+    attributions: ' ',
+});
+jsonSource_Maillage_Effort_mis_a_jour_le_17_janvier_2024_4.addFeatures(features_Maillage_Effort_mis_a_jour_le_17_janvier_2024_4);
+var lyr_Maillage_Effort_mis_a_jour_le_17_janvier_2024_4 = new ol.layer.Vector({
+                declutter: true,
+                source:jsonSource_Maillage_Effort_mis_a_jour_le_17_janvier_2024_4, 
+                style: style_Maillage_Effort_mis_a_jour_le_17_janvier_2024_4,
+                interactive: true,
+    title: 'Maillage_Effort -_mis_a_jour_le_17_janvier_2024<br />\
+    <img src="styles/legend/Maillage_Effort_mis_a_jour_le_17_janvier_2024_4_0.png" /> 0<br />\
+    <img src="styles/legend/Maillage_Effort_mis_a_jour_le_17_janvier_2024_4_1.png" /> 1 - 2<br />'
+        });
+
+lyr_ScanRgionalFXXWM082021_0.setVisible(true);lyr_DPT_AURA_PACA_1.setVisible(true);lyr_ZPP_ETE_2023_2.setVisible(false);lyr_Maillage_Effort_mis_a_jour_le_17_janvier_2024_3.setVisible(true);lyr_Maillage_Effort_mis_a_jour_le_17_janvier_2024_4.setVisible(true);
+var layersList = [lyr_ScanRgionalFXXWM082021_0,lyr_DPT_AURA_PACA_1,lyr_ZPP_ETE_2023_2,lyr_Maillage_Effort_mis_a_jour_le_17_janvier_2024_3,lyr_Maillage_Effort_mis_a_jour_le_17_janvier_2024_4];
+lyr_DPT_AURA_PACA_1.set('fieldAliases', {'code_insee': 'code_insee', 'nom': 'nom', });
+lyr_ZPP_ETE_2023_2.set('fieldAliases', {'polygone_2': 'polygone_2', 'Id': 'Id', 'ETE_23': 'ETE_23', });
+lyr_Maillage_Effort_mis_a_jour_le_17_janvier_2024_3.set('fieldAliases', {'CODE_10KM': 'CODE_10KM', 'Nb_Indices': 'Nb_Indices', });
+lyr_Maillage_Effort_mis_a_jour_le_17_janvier_2024_4.set('fieldAliases', {'CODE_10KM': 'CODE_10KM', 'Nb_Indices': 'Nb_Indices', });
+lyr_DPT_AURA_PACA_1.set('fieldImages', {'code_insee': 'TextEdit', 'nom': 'TextEdit', });
+lyr_ZPP_ETE_2023_2.set('fieldImages', {'polygone_2': 'TextEdit', 'Id': 'Range', 'ETE_23': 'Range', });
+lyr_Maillage_Effort_mis_a_jour_le_17_janvier_2024_3.set('fieldImages', {'CODE_10KM': 'TextEdit', 'Nb_Indices': 'TextEdit', });
+lyr_Maillage_Effort_mis_a_jour_le_17_janvier_2024_4.set('fieldImages', {'CODE_10KM': 'TextEdit', 'Nb_Indices': 'TextEdit', });
+lyr_DPT_AURA_PACA_1.set('fieldLabels', {'code_insee': 'no label', 'nom': 'no label', });
+lyr_ZPP_ETE_2023_2.set('fieldLabels', {'polygone_2': 'no label', 'Id': 'no label', 'ETE_23': 'no label', });
+lyr_Maillage_Effort_mis_a_jour_le_17_janvier_2024_3.set('fieldLabels', {'CODE_10KM': 'inline label', 'Nb_Indices': 'inline label', });
+lyr_Maillage_Effort_mis_a_jour_le_17_janvier_2024_4.set('fieldLabels', {'CODE_10KM': 'inline label', 'Nb_Indices': 'inline label', });
+lyr_Maillage_Effort_mis_a_jour_le_17_janvier_2024_4.on('precompose', function(evt) {
+    evt.context.globalCompositeOperation = 'normal';
+});
---
+++ styles/ZPP_ETE_2023_2_style.js
@@ -0,0 +1,48 @@
+var size = 0;
+var placement = 'point';
+function categories_ZPP_ETE_2023_2(feature, value, size, resolution, labelText,
+                       labelFont, labelFill, bufferColor, bufferWidth,
+                       placement) {
+                switch(value.toString()) {case '1':
+                    return [ new ol.style.Style({
+        stroke: new ol.style.Stroke({color: 'rgba(22,48,238,1.0)', lineDash: null, lineCap: 'square', lineJoin: 'bevel', width: 2}),
+        text: createTextStyle(feature, resolution, labelText, labelFont,
+                              labelFill, placement, bufferColor,
+                              bufferWidth)
+    })];
+                    break;
+case '4':
+                    return [ new ol.style.Style({
+        stroke: new ol.style.Stroke({color: 'rgba(172,32,232,1.0)', lineDash: null, lineCap: 'square', lineJoin: 'bevel', width: 2}),
+        text: createTextStyle(feature, resolution, labelText, labelFont,
+                              labelFill, placement, bufferColor,
+                              bufferWidth)
+    })];
+                    break;}};
+
+var style_ZPP_ETE_2023_2 = function(feature, resolution){
+    var context = {
+        feature: feature,
+        variables: {}
+    };
+    var value = feature.get("ETE_23");
+    var labelText = "";
+    size = 0;
+    var labelFont = "13.0px \'Liberation Sans\', sans-serif";
+    var labelFill = "#323232";
+    var bufferColor = "";
+    var bufferWidth = 0;
+    var textAlign = "left";
+    var offsetX = 8;
+    var offsetY = 3;
+    var placement = 'point';
+    if ("" !== null) {
+        labelText = String("");
+    }
+    
+var style = categories_ZPP_ETE_2023_2(feature, value, size, resolution, labelText,
+                          labelFont, labelFill, bufferColor,
+                          bufferWidth, placement);
+
+    return style;
+};
